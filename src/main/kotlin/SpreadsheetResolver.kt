7981889589567e9698e32e4e2e15c2bf1fa3d6d2
Stefan Herz <stefan.herz@thinktable.de>
2017-12-05 18:46:18 +0100

    solved puzzle 2.2!

--- src/main/kotlin/SpreadsheetResolver.kt
+++ src/main/kotlin/SpreadsheetResolver.kt
@@ -19,9 +19,18 @@ object SpreadsheetResolver {
         val listIterator = input.split("\n").listIterator()
         while (listIterator.hasNext()) {
             val row = listIterator.next().split("\t").map({ s -> s.toInt() })
-
+            row.forEach({number -> sum += checkRest(number, row)})
         }
         return sum
+    }
+
+    private fun checkRest(number: Int, row: List<Int>): Int {
+        val others = row.filter({ currentNumber -> currentNumber != number })
+        val divisor = others.find({ n -> (number % n) == 0 })
+        if(divisor != null) {
+            return number/divisor
+        }
+        return 0
     }
 
 }
