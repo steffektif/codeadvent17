2cd2325fa6821a2ecfbf38a2a5b20901edc6b166
Stefan Herz <stefan.herz@signavio.com>
2017-12-24 14:12:06 +0100

    solved day 3 part 2

--- src/main/kotlin/SpiralResolver.kt
+++ src/main/kotlin/SpiralResolver.kt
@@ -1,7 +1,8 @@
 class SpiralResolver {
 
-    private val sequence = sortedSetOf(Direction.RIGHT, Direction.UP, Direction.LEFT, Direction.DOWN)
-
+    private val sequence = linkedSetOf(Direction.RIGHT, Direction.UP, Direction.LEFT, Direction.DOWN)
+    private var positionGrid: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
+    private var lastValue: Int = 1
 
     fun findCoordinates(goal: Int): Pair<Int, Int> {
         var range = 1
@@ -12,6 +13,7 @@ class SpiralResolver {
         var directionChanged = 0
 
         var directionToTake = Direction.RIGHT
+
         val direction = sortedMapOf(
                 Pair(Direction.RIGHT, this::goRight),
                 Pair(Direction.UP, this::gotUp),
@@ -33,6 +35,64 @@ class SpiralResolver {
             }
         }
         return position
+    }
+
+    fun findCoordinatesModified(goal: Int): Int {
+        positionGrid.put(Pair(0,0), 1)
+        var range = 1
+        var stepsTaken = 1
+        var stepsInThisDirection = 0
+        var position = Pair(0, 0)
+
+        var directionChanged = 0
+
+        var directionToTake = Direction.RIGHT
+        val direction = sortedMapOf(
+                Pair(Direction.RIGHT, this::goRight),
+                Pair(Direction.UP, this::gotUp),
+                Pair(Direction.LEFT, this::goLeft),
+                Pair(Direction.DOWN, this::goDown))
+
+        while (lastValue <= goal) {
+            while (lastValue <= goal && stepsInThisDirection != range) {
+                position = direction[directionToTake]?.invoke(position)!!
+                lastValue = calculateValue(position)
+                positionGrid.put(position, lastValue)
+                stepsInThisDirection++
+                stepsTaken++
+//                println(position)
+//                println(lastValue)
+            }
+            stepsInThisDirection = 0
+            directionToTake = getNext(directionToTake)
+            directionChanged++
+            if (directionChanged == 2) {
+                directionChanged = 0
+                range++
+            }
+        }
+        println(positionGrid)
+        return lastValue
+    }
+
+    private fun calculateValue(position: Pair<Int, Int>): Int {
+//        if(position.first == 0 && position.second == 0) {
+//            return 1
+//        }
+        val map = Direction.values().map { value -> findSumValue(position, value) }
+        return map.sum()
+    }
+
+    private fun findSumValue(position: Pair<Int, Int>, direction: Direction): Int {
+        val modifiedX = position.first + direction.addedPosition.first
+        val modifiedY = position.second + direction.addedPosition.second
+
+        var sum = 0
+        val found = positionGrid[Pair(modifiedX, modifiedY)]
+        if (found != null) {
+            sum = found
+        }
+        return sum
     }
 
     private fun goRight(actual: Pair<Int, Int>): Pair<Int, Int> {
@@ -65,8 +125,7 @@ class SpiralResolver {
         return sequence.elementAt(index.inc())
     }
 
-    fun goToCenter(position: Pair<Int,Int>): Int {
-        println(position)
+    fun goToCenter(position: Pair<Int, Int>): Int {
         var first = position.first
         var second = position.second
         if (first.toString().startsWith("-")) first = first.unaryMinus()
@@ -74,8 +133,8 @@ class SpiralResolver {
         return first + second
     }
 
-    enum class Direction {
-        LEFT, RIGHT, UP, DOWN;
+    enum class Direction(val addedPosition: Pair<Int, Int>) {
+        LEFT(Pair(-1, 0)), RIGHT(Pair(1, 0)), UP(Pair(0, 1)), DOWN(Pair(0, -1)),
+        TOP_LEFT(Pair(-1, 1)), TOP_RIGHT(Pair(1, 1)), BOTTOM_LEFT(Pair(-1, -1)), BOTTOM_RIGHT(Pair(1, -1));
     }
-
 }
--- src/test/kotlin/SpiralResolverTest.kt
+++ src/test/kotlin/SpiralResolverTest.kt
@@ -8,10 +8,10 @@ import org.junit.runner.RunWith
 
 @RunWith(JUnitPlatform::class)
 class SpiralResolverTest : Spek({
-    val resolver = SpiralResolver()
 
     describe("A SpiralResolver") {
         on("a given tile number of 1") {
+            val resolver = SpiralResolver()
             it("should return the coordinates 0/0") {
                 val result = resolver.findCoordinates(1)
                 TestCase.assertEquals(Pair(0, 0), result)
@@ -19,6 +19,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("a given tile number of 12") {
+            val resolver = SpiralResolver()
             it("should return the coordinates 2/1") {
                 val result = resolver.findCoordinates(12)
                 TestCase.assertEquals(Pair(2, 1), result)
@@ -26,6 +27,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("a given tile number of 23") {
+            val resolver = SpiralResolver()
             it("should return the coordinates 0/-2") {
                 val result = resolver.findCoordinates(23)
                 TestCase.assertEquals(Pair(0, -2), result)
@@ -33,6 +35,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("the given position of 0/0") {
+            val resolver = SpiralResolver()
             it("should return the number of steps: 0") {
                 val result = resolver.goToCenter(Pair(0, 0))
                 TestCase.assertEquals(0, result)
@@ -40,6 +43,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("the given position of 2/1") {
+            val resolver = SpiralResolver()
             it("should return the number of steps: 3") {
                 val result = resolver.goToCenter(Pair(2, 1))
                 TestCase.assertEquals(3, result)
@@ -47,6 +51,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("the given position of 0/-2") {
+            val resolver = SpiralResolver()
             it("should return the number of steps: 2") {
                 val result = resolver.goToCenter(Pair(0,-2))
                 TestCase.assertEquals(2, result)
@@ -54,6 +59,7 @@ class SpiralResolverTest : Spek({
         }
 
         on("the given input of 1024") {
+            val resolver = SpiralResolver()
             it ("should return 31 steps") {
                 val result = resolver.goToCenter(resolver.findCoordinates(1024))
                 TestCase.assertEquals(31, result)
@@ -61,11 +67,21 @@ class SpiralResolverTest : Spek({
         }
 
         on("actual puzzle input of 312051") {
+            val resolver = SpiralResolver()
             it("should return the correct answer :)") {
                 val result = resolver.goToCenter(resolver.findCoordinates(312051))
                 println(result)
             }
         }
+    }
 
+    describe("a spiral resolver") {
+        on("actual puzzle input of 312051") {
+            val resolver = SpiralResolver()
+            it("should return the next bigger value written") {
+                val result = resolver.findCoordinatesModified(312051)
+                println(result)
+            }
+        }
     }
 })
