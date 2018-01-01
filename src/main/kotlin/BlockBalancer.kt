f0f26bd68513627731962ef755c11cd642d8ba16
Stefan Herz <stefan.herz@signavio.com>
2018-01-01 13:40:27 +0100

    solved day 6

---
+++ src/main/kotlin/BlockBalancer.kt
@@ -0,0 +1,55 @@
+
+fun main(args: Array<String>) {
+    val input = "2\t8\t8\t5\t4\t2\t3\t1\t5\t5\t1\t2\t15\t13\t5\t14"
+    val blockBalancer = BlockBalancer()
+    val result = blockBalancer.findInfiniteLoop(input)
+    println(result)
+}
+
+class BlockBalancer {
+
+    fun findInfiniteLoop(input: String): Int {
+        val blocks = input.split("\t")
+                .filter { char -> char != "" }
+                .map { char -> char.toInt() }
+                .toMutableList()
+        val knownPatterns = mutableSetOf<List<Int>>(blocks)
+
+        var copy = blocks.toMutableList() // make a copy
+        while (true) {
+            val redistributedBlocks = redistribute(copy)
+            if (knownPatterns.contains(redistributedBlocks)) {
+//                return knownPatterns.size
+                val matchingPattern = redistributedBlocks.toList()
+                var counter = 0
+                while (true) {
+                    val innerLoopBlocks = redistribute(copy)
+                    counter = counter.inc()
+                    if(innerLoopBlocks == matchingPattern) {
+                        return counter
+                    }
+                }
+            } else {
+                knownPatterns.add(redistributedBlocks)
+            }
+            copy = redistributedBlocks
+        }
+    }
+
+    private fun redistribute(blocks: MutableList<Int>): MutableList<Int> {
+        var max = blocks.max()
+        var index = blocks.indexOf(max) // first one wins
+        blocks[index] = 0
+
+        while (max != 0) {
+            index = index.inc()
+            if(index == blocks.size) {
+                index = 0
+            }
+            blocks[index] = blocks[index].inc()
+            max = max!!.dec()
+        }
+        return blocks
+    }
+
+}
---
+++ src/test/kotlin/BlockBalanceTest.kt
@@ -0,0 +1,16 @@
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.Assert.assertEquals
+
+class BlockBalanceTest : Spek({
+    describe("A block balancer") {
+        on("the input of 0 2 7 0") {
+            it("should return 5") {
+                val redistributions = BlockBalancer().findInfiniteLoop("0\t2\t7\t0")
+                assertEquals(5, redistributions)
+            }
+        }
+    }
+})
